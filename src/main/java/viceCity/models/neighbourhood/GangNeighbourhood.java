d65d3a7f49fecc67d5e92022fdc9120a12a0d072
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-19 18:42:09 +0200

    add

--- src/main/java/viceCity/models/neighbourhood/GangNeighbourhood.java
+++ src/main/java/viceCity/models/neighbourhood/GangNeighbourhood.java
@@ -2,9 +2,19 @@ package viceCity.models.neighbourhood;
 
 import viceCity.models.players.Player;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class GangNeighbourhood implements Neighbourhood{
+
+    private Player mainPlayer;
+    private Collection<Player> civilPlayers;
+
+    public GangNeighbourhood() {
+        this.mainPlayer = mainPlayer;
+        this.civilPlayers = new ArrayList<Player>();
+    }
+
     @Override
     public void action(Player mainPlayer, Collection<Player> civilPlayers) {
 
--- src/main/java/viceCity/repositories/interfaces/GunRepository.java
+++ src/main/java/viceCity/repositories/interfaces/GunRepository.java
@@ -2,26 +2,43 @@ package viceCity.repositories.interfaces;
 
 import viceCity.models.guns.Gun;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class GunRepository implements Repository<Gun>{
+
+    private Collection<Gun> models;
+
+    public GunRepository() {
+        this.models = new ArrayList<Gun>();
+    }
+
     @Override
     public Collection<Gun> getModels() {
-        return null;
+        return Collections.unmodifiableCollection(models);
     }
 
     @Override
     public void add(Gun model) {
-
+        if (!models.contains(model)) {
+            models.add(model);
+        }
     }
 
     @Override
     public boolean remove(Gun model) {
+        if (models.contains(model)) {
+            models.remove(model);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Gun find(String name) {
-        return null;
+        return models.stream()
+                .filter(gun -> gun.getName().equals(name)).findFirst()
+                .orElse(null);
     }
 }
